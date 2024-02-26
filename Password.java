5003cd9b52d9ca4fff033eef3346c9ccbbf21bd9
furquan5 <furquanthejustice555@gmail.com>
2024-02-26 11:38:33 +0530

    Your descriptive commit message here

---
+++ Password.java
@@ -0,0 +1,30 @@
+class Password{
+    private int number;
+    private String alphabet;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getAlphabet() {
+        return alphabet;
+    }
+
+    public void setAlphabet(String alphabet) {
+        this.alphabet = alphabet;
+    }
+
+    public string getSpecialCharacter() {
+        return specialCharacter;
+    }
+
+    public void setSpecialCharacter(string specialCharacter) {
+        this.specialCharacter = specialCharacter;
+    }
+
+    private string specialCharacter;
+}
